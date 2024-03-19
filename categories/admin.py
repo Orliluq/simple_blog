5af5cdc319d72aca714973bdcb436c4c723ab819
Orli Dun <122529721+Orliluq@users.noreply.github.com>
2024-03-19 16:08:40 -0400

    Add files via upload

---
+++ categories/admin.py
@@ -0,0 +1,13 @@
+#from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from categories.models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin."""
+
+    list_display = ('id', 'name')
